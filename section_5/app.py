150804178e6f96af2a779e038f7ab89c2f83ff36
Matt Pearson <pearson.matt345@gmail.com>
2021-04-26 22:53:06 -0600

    Adding simple endpoints to the api

--- section_5/app.py
+++ section_5/app.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template
+from flask_restful import Api, Resource
 
 app = Flask (__name__)
+api = Api (app)
 
-@app.route ('/')
-def home ():
-  return render_template ('index.html')
+items = []
+
+class Item (Resource):
+  def get (self, name):
+    return {'item': next (filter (lambda x: x['name'] == name, items), None)}
+
+  def put (self, name):
+    if (next (filter (lambda x: x['name'] == name, items), None) is not None):
+      return {'message': 'name already exists in items'} 
+
+    item = {'name': name, 'value': 'random_value'}
+    items.append(item)
+    return {'item': item}
+
+class ItemList (Resource):
+  def get (self):
+    return {'items': items} 
+
+api.add_resource (Item, '/item/<string:name>')
+api.add_resource (ItemList, '/items')
 
 app.run (host="0.0.0.0", port=5000, debug=True)
